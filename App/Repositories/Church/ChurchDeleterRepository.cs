ed18aefa66d28ffa7bbbbd54b4894e66c27d208b
Ricardo Alves Kindermann Junior <almirantekindermann@hotmail.com>
2021-11-24 05:42:56 -0300

    build: implementação do ChurchDeleterRepository

---
+++ App/Repositories/Church/ChurchDeleterRepository.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Church.App.Repositories.Church
+{
+    class ChurchDeleterRepository : IChurchDeleterRepository
+    {
+        public bool InactiveChurch(int id_church)
+        {
+            bool success = false;
+
+            using (MySqlConnection mySqlConnection = new MySqlConnection(/*conexão com o banco*/))
+            {
+                MySqlCommand mySqlCommand = new MySqlCommand();
+                mySqlCommand.CommandText = $"UPDATE church SET cadasterisactive = 'Inativo' WHERE id = {id_church}";
+                int affectedRows = mySqlCommand.ExecuteNonQuery();
+                success = affectedRows > 0;
+            }
+            return success;
+        }
+    }
+}
